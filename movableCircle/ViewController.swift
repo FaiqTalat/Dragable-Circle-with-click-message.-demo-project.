ca382a0fdd710ecbb4d4464663e761b216a36e5a
Faiq Talat <faiktalat@gmail.com>
2016-01-07 10:19:18 -0800

    project created.

--- movableCircle/ViewController.swift
+++ movableCircle/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var movableCircle: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // for moving...
+        let panGesture = UIPanGestureRecognizer(target: self, action: "touchMoved:")
+        self.movableCircle.addGestureRecognizer(panGesture)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "tapOnCircle")
+        self.movableCircle.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+    
+        self.movableCircle.layer.cornerRadius = self.movableCircle.frame.width / 2
+        
+        self.movableCircle.translatesAutoresizingMaskIntoConstraints()
+        
     }
 
+    func touchMoved(sender: UIPanGestureRecognizer){
+    let location = sender.locationInView(self.view)
+        println("location: \(location), sender.state: \(sender.state.hashValue)")
+        self.movableCircle.center = location
+        
+    }
+    func tapOnCircle(){
+        let alert = UIAlertController(title: "DemoTitle", message: "Demo Message", preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "GotIt", style: UIAlertActionStyle.Default) { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
